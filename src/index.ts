149252adc8ba427606dd286c00a0b92d7cc5b1ce
Damian Esteban <damian.esteban@gmail.com>
2022-05-26 20:23:23 -0400

    feat: ok the df

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ function* counter(): Generator<number, string, boolean> {
       break;
     }
   }
-  return "aaaaaahhaaaaaaaaaaaaaaaadone!";
+  return "aaaaaahhauuuuuuuuuuuuaaaaaaaaaaaaaaadone!";
 }
 var iter = counter();
 var curr = iter.next();
